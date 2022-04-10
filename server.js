2f95af227df7548c472b42b91a6e96517e5327bc
CriticalHugo <hugomissionario@gmail.com>
2022-04-10 15:07:25 -0300

    First Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fetch = require("cross-fetch");
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Port: ${port}`));
+
+app.get('/api/piada', (req, res) => {
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then((resp) => resp.json())
+        .then(function(data) {
+            res.send(`Piada: ${data.value}`);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+});
